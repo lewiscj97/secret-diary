f905c0c42a038f094986dc466bdadebaaf956d55
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-27 12:03:58 +0100

    test(secret_diary): test for exception when adding entry to locked diary

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -6,4 +6,12 @@ describe SecretDiary do
       expect(subject.status).to eq "locked"
     end
   end
+
+  describe "#add_entry" do
+    context "diary is locked" do
+      it "does not let the user add an entry" do
+        expect { subject.add_entry }.to raise_error "Diary is locked"
+      end
+    end
+  end
 end
